c90523716866e7784f29f1ac932ca0f4b24e48d8
Tomoaki Imai <tomoima525@gmail.com>
2024-01-11 12:05:30 -0800

    add a guide for custom nat instance

--- lib/development-template-stack.ts
+++ lib/development-template-stack.ts
@@ -5,15 +5,18 @@ import {
   Stack,
   StackProps,
   CfnOutput,
-  Duration,
+  RemovalPolicy,
 } from "aws-cdk-lib";
+
 import { Construct } from "constructs";
 import * as path from "path";
+import * as fs from "fs";
 
 export class DevelopmentTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Pattern 1 - NAT Gateway from AMI
     const natIntance = ec2.NatProvider.instance({
       machineImage: new ec2.GenericLinuxImage({
         // NAT instance AMI
@@ -63,9 +66,68 @@ export class DevelopmentTemplateStack extends Stack {
       instanceId: natInstance2.instanceId,
     });
 
+    // Pattern 2 - Your own NAT instance
+    const vpc2 = new ec2.Vpc(this, "VPC2", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: "private-",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 28,
+        },
+        {
+          name: "public-",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 28,
+        },
+      ],
+    });
+    // Security group -- allow ssh
+    const publicSg = new ec2.SecurityGroup(this, "public-sg", {
+      vpc: vpc2,
+      allowAllOutbound: true,
+    });
+
+    publicSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "allow ssh access",
+    );
+
+    const customNat = new ec2.Instance(this, `custom-nat-instance`, {
+      vpc: vpc2,
+      securityGroup: publicSg,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      keyName: "testkeypair2", // replace with your own keypair
+      sourceDestCheck: false, // should be false for NAT instances
+      associatePublicIpAddress: true, // assigns public IPs to Subnets
+    });
+
+    // Inject intial script to setup iptable routing for NAT
+    const initScriptPath = path.join(`${__dirname}/`, "init-script.sh");
+    const userData = fs.readFileSync(initScriptPath, "utf8");
+    customNat.addUserData(userData);
+
+    customNat.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
     new lambda_nodejs.NodejsFunction(this, "CurrentIP", {
       runtime: lambda.Runtime.NODEJS_18_X,
       vpc,
+      handler: "handler",
+      entry: path.join(`${__dirname}/../`, "functions", "current-ip/index.ts"),
+      bundling: {
+        format: lambda_nodejs.OutputFormat.ESM,
+      },
+    });
+    new lambda_nodejs.NodejsFunction(this, "CurrentIP2", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      vpc: vpc2,
       handler: "handler",
       entry: path.join(`${__dirname}/../`, "functions", "current-ip/index.ts"),
       bundling: {
